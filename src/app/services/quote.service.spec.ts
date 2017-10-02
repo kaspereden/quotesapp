69926cb77246bf92e2004b20a1457faae6966538
Kasper Reijnders <kasper.reijnders@incentro.com>
2017-10-02 20:45:45 +0200

    added 2 tests to check the type of the responseπ

--- src/app/services/quote.service.spec.ts
+++ src/app/services/quote.service.spec.ts
@@ -1,17 +1,36 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject, async} from '@angular/core/testing';
 
-import { QuoteService } from './quote.service';
-import {HttpModule} from '@angular/http';
+import {QuoteService} from './quote.service';
+import {HttpModule, ResponseOptions, XHRBackend} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+import {Quote} from '../models/quote';
 
 describe('QuoteService', () => {
+  let service: QuoteService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [QuoteService],
+      providers: [QuoteService, {provide: XHRBackend, useClass: MockBackend}
+      ],
       imports: [HttpModule]
     });
+
+    service = TestBed.get(QuoteService, null);
   });
 
-  it('should be created', inject([QuoteService], (service: QuoteService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('getRandomQuote should return Promise<Quote>', async () => {
+    service.getRandomQuote().then(
+      quote => expect(typeof quote).toEqual('Expected<Quote>')
+    );
+  });
+
+  it('getQuoteById should return Promise<Quote>', async () => {
+    service.getQuoteById(7).then(
+      quote => expect(typeof quote).toEqual('Expected<Quote>')
+    );
+  });
 });
